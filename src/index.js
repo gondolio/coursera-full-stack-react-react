8e8964a4252b828a2a1628a5ae715cb093f7729c
gondolio <oren.sivan@gmail.com>
2019-02-14 18:07:02 +0900

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css'; // This comes after so we can override bootstrap
 import App from './App';
 import * as serviceWorker from './serviceWorker';
