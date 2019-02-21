0b83259f8068a9e44c360ee5bc0071825fc1a8a5
gondolio <oren.sivan@gmail.com>
2019-02-21 10:05:44 +0900

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -50,9 +51,10 @@ class Main extends Component {
       <Header />
       <Switch>
         <Route path="/home" component={HomePage} />
+        <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
         <Route path="/menu/:dishId" component={DishWithId} />
-        <Route exact path='/contactus' component={Contact} />} />
+        <Route exact path='/contactus' component={Contact} />} />        
         <Redirect to="/home" />
       </Switch>
       <Footer />
