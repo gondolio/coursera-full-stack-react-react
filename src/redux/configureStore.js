cbf370183ac5ead739132356adb735d5d8e16c61
gondolio <oren.sivan@gmail.com>
2019-03-04 06:52:46 +0900

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Comments } from './comments';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+import { DISHES } from '../shared/dishes';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            comments: Comments,
+            dishes: Dishes,
+            leaders: Leaders,
+            promotions: Promotions,
+        }),
     );
 
     return store;
