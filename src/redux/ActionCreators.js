d0b7eece6608d699ceecee16e9a46095848a2f1b
gondolio <oren.sivan@gmail.com>
2019-03-05 08:17:28 +0900

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,28 @@ export const addComment = (dishId, rating, author, comment) => ({
         dishId: dishId,
         rating: rating,
     }
+});
+
+// Thunk lets us return a function instead of an action
+// This can be useful for more complex actions, such as in fetchDishes below
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true))
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000); // Simulating an async server call
+};
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes,
+})
+
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess,
 });
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Comments } from './comments';
 import { Dishes } from './dishes';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'; // Logs action nicely to console (prev state, action, next state)
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,6 +14,7 @@ export const ConfigureStore = () => {
             leaders: Leaders,
             promotions: Promotions,
         }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
